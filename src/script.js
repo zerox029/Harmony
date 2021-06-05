e46888d5c0444272903a434ad5385f1afdc26b97
Étienne Plante <zerox029@gmail.com>
2021-06-05 16:23:29 -0400

    :sparkles: Added MIDI support

--- src/script.js
+++ src/script.js
@@ -1,14 +1,42 @@
 import Vex from 'vexflow';
+import * as JZZ from 'jzz'
+import { Midi } from "@tonaljs/tonal"
 
-var VF = Vex.Flow;
+const VF = Vex.Flow;
 
-var div = document.getElementById("vf")
-var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+// Create an SVG renderer and attach it to the DIV element named "vf".
+const div = document.getElementById("vf")
+const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+// Configure the rendering context.
 renderer.resize(500, 500);
+const context = renderer.getContext();
+context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
-var context = renderer.getContext();
+function renderGrandStaff() {
+    // Create a stave of width 400 at position 10, 40 on the canvas.
+    var topStaff = new Vex.Flow.Stave(20, 0, 200);
+    var bottomStaff = new Vex.Flow.Stave(20, 100, 200);
 
-var stave = new VF.Stave(10, 40, 400);
-stave.addClef("treble");
+    // Add a clef and time signature.
+    topStaff.addClef('treble');
+    bottomStaff.addClef('bass');
 
-stave.setContext(context).draw();
+    var brace = new Vex.Flow.StaveConnector(topStaff, bottomStaff).setType(3); // 3 = brace
+    var lineRight = new Vex.Flow.StaveConnector(topStaff, bottomStaff).setType(6);
+    var lineLeft = new Vex.Flow.StaveConnector(topStaff, bottomStaff).setType(1);
+
+    // Drawing
+    topStaff.setContext(context).draw();
+    bottomStaff.setContext(context).draw();
+
+    brace.setContext(context).draw();
+    lineRight.setContext(context).draw();
+    lineLeft.setContext(context).draw();
+}
+
+renderGrandStaff();
+
+var input = JZZ().openMidiIn();
+var delay = JZZ.Widget({ _receive: function(msg) { console.log(Midi.midiToNoteName(msg[1])) }});
+input.connect(delay);
