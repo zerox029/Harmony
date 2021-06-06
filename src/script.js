3e1e41e59c797466ca4a316e2cd8caf7cb8e0d4f
Étienne Plante <zerox029@gmail.com>
2021-06-06 11:49:01 -0400

    Added basic single note rendering

--- src/script.js
+++ src/script.js
@@ -4,27 +4,35 @@ import { Midi } from "@tonaljs/tonal"
 
 const VF = Vex.Flow;
 
-// Create an SVG renderer and attach it to the DIV element named "vf".
-const div = document.getElementById("vf")
-const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+function setupRenderer() {
+    // Create an SVG renderer and attach it to the DIV element named "vf".
+    const div = document.getElementById("vf")
+    const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+    
+    // Configure the rendering context.
+    renderer.resize(500, 500);
+    const context = renderer.getContext();
+    context.setFont("Arial", 20, "").setBackgroundFillStyle("#eed");
 
-// Configure the rendering context.
-renderer.resize(500, 500);
-const context = renderer.getContext();
-context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+    return context;
+}
+
+function render(notes) {
+    let staves = renderGrandStaff();
+    renderNotes(staves, notes);
+}
 
 function renderGrandStaff() {
-    // Create a stave of width 400 at position 10, 40 on the canvas.
-    var topStaff = new Vex.Flow.Stave(20, 0, 200);
-    var bottomStaff = new Vex.Flow.Stave(20, 100, 200);
+    context.rect(0, 0, 500, 500, { stroke: 'none', fill: 'white' });
 
-    // Add a clef and time signature.
+    let topStaff = new Vex.Flow.Stave(20, 0, 200);
     topStaff.addClef('treble');
+    let bottomStaff = new Vex.Flow.Stave(20, 100, 200);
     bottomStaff.addClef('bass');
 
-    var brace = new Vex.Flow.StaveConnector(topStaff, bottomStaff).setType(3); // 3 = brace
-    var lineRight = new Vex.Flow.StaveConnector(topStaff, bottomStaff).setType(6);
-    var lineLeft = new Vex.Flow.StaveConnector(topStaff, bottomStaff).setType(1);
+    let brace = new Vex.Flow.StaveConnector(topStaff, bottomStaff).setType(3);
+    let lineRight = new Vex.Flow.StaveConnector(topStaff, bottomStaff).setType(6);
+    let lineLeft = new Vex.Flow.StaveConnector(topStaff, bottomStaff).setType(1);
 
     // Drawing
     topStaff.setContext(context).draw();
@@ -33,10 +41,50 @@ function renderGrandStaff() {
     brace.setContext(context).draw();
     lineRight.setContext(context).draw();
     lineLeft.setContext(context).draw();
+
+    return [topStaff, bottomStaff];
 }
 
-renderGrandStaff();
+function renderNotes(staves, notes) {
+    var voice = new VF.Voice({num_beats: 1,  beat_value: 4});
+    voice.addTickables(notes);
 
-var input = JZZ().openMidiIn();
-var delay = JZZ.Widget({ _receive: function(msg) { console.log(Midi.midiToNoteName(msg[1])) }});
-input.connect(delay);
+    var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 130);
+
+    voice.draw(context, staves[0])
+}
+
+function noteNameToVfName(name) {
+    let characters = name.split("");
+
+    switch(characters.length) {
+        case 2:
+            return `${characters[0].toLowerCase()}/${characters[1]}`
+        case 3:
+            return `${characters[0].toLowerCase()}${characters[1]}/${characters[2]}`
+    }
+}
+
+function onMidiSignal(msg)
+{
+    let currentNote = noteNameToVfName(Midi.midiToNoteName(msg[1]));
+
+    let notes = [];
+
+    if(currentNote[1] !== "/")
+        notes = [new VF.StaveNote({ clef: "treble", keys: [currentNote], duration: "q", align_center: true }).addAccidental(0, new VF.Accidental(currentNote[1]))];
+    else 
+        notes = [new VF.StaveNote({ clef: "treble", keys: [currentNote], duration: "q", align_center: true })];
+
+    render(notes);
+}
+
+function setupMidi() {
+    let input = JZZ().openMidiIn();
+    let delay = JZZ.Widget({ _receive: function(msg) { onMidiSignal(msg) }});
+    input.connect(delay);    
+}
+
+const context = setupRenderer();
+renderGrandStaff(context);
+setupMidi();
